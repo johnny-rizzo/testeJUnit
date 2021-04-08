38c4d5108888d5412a3add6653a6ef88a88742dc
johnny <jal.johnny13@mail.com>
2021-04-08 17:13:07 -0300

    testes

---
+++ BancodeDados/src/main/java/com/BancodeDados/BancodeDados/BancodeDadosApplication.java
@@ -0,0 +1,13 @@
+package com.BancodeDados.BancodeDados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancodeDadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancodeDadosApplication.class, args);
+	}
+
+}
---
+++ BancodeDados/src/test/java/com/BancodeDados/BancodeDados/BancodeDadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BancodeDados.BancodeDados;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+ class BancodeDadosApplicationTests {
+
+	@Test
+	 void contextLoads() {
+	}
+
+}
